79d0c743e9f7b680d00398c26f3e4024fac53c93
Esad <esad@Esad-MacBook-Pro.local>
2020-10-25 04:50:50 +0300

    homepage

--- HomeStack.js
+++ HomeStack.js
@@ -23,13 +23,17 @@ function HomePage({ navigation }) {
 
       {/* Expenses Chart */}
 
-      <Box p={8} flex={1} flexDirection={'column'} bg="">
+      <Box p={8} flex={1} flexDirection={'column'} >
 
         {/* Content Box */}
-        <Box as={Button} flex={1} flexDirection={'column'} bg=""
+        <Box as={Button} flex={1} flexDirection={'column'}
 
           alignItems="flex-start" onPress={() => navigation.navigate('Expenses')}>
-          <Text>Esad</Text>
+          <Text mb={2} >Expenses</Text>
+          <Box flexDirection={'row'}>
+            <Box mb={2} height={2} flex={1} bg={'black'}  >
+            </Box>
+          </Box>
           <Text>Esad</Text>
           <Text>Esad</Text>
           <Text>Esad</Text>
@@ -38,7 +42,7 @@ function HomePage({ navigation }) {
 
         {/* Add Button */}
         <Box alignItems="flex-end">
-          <Box as={Button} mt={-24} width={48} height={48} bg={''} borderRadius='full'  >
+          <Box as={Button} mt={-24} width={48} height={48} borderRadius='full'  >
             <AddCircle24Px width={32} height={32} fill={'black'} />
           </Box>
         </Box>
@@ -46,10 +50,58 @@ function HomePage({ navigation }) {
 
 
       {/* Chores */}
+      <Box p={8} flex={1} flexDirection={'column'} >
 
+        {/* Content Box */}
+        <Box as={Button} flex={1} flexDirection={'column'}
+
+          alignItems="flex-start" onPress={() => navigation.navigate('Chores')}>
+          <Text mb={2} >Chores</Text>
+          <Box flexDirection={'row'}>
+            <Box mb={2} height={2} flex={1} bg={'black'}  >
+            </Box>
+          </Box>
+          <Text>Esad</Text>
+          <Text>Esad</Text>
+          <Text>Esad</Text>
+          <Text>Esad</Text>
+        </Box>
+
+        {/* Add Button */}
+        <Box alignItems="flex-end">
+          <Box as={Button} mt={-24} width={48} height={48} borderRadius='full'  >
+            <AddCircle24Px width={32} height={32} fill={'black'} />
+          </Box>
+        </Box>
+      </Box>
       {/* Chores */}
 
       {/* List */}
+
+      <Box p={8} flex={1} flexDirection={'column'} >
+
+        {/* Content Box */}
+        <Box as={Button} flex={1} flexDirection={'column'}
+
+          alignItems="flex-start" onPress={() => navigation.navigate('List')}>
+          <Text mb={2} >Need List</Text>
+          <Box flexDirection={'row'}>
+            <Box mb={2} height={2} flex={1} bg={'black'}  >
+            </Box>
+          </Box>
+          <Text>Esad</Text>
+          <Text>Esad</Text>
+          <Text>Esad</Text>
+          <Text>Esad</Text>
+        </Box>
+
+        {/* Add Button */}
+        <Box alignItems="flex-end">
+          <Box as={Button} mt={-24} width={48} height={48} borderRadius='full'  >
+            <AddCircle24Px width={32} height={32} fill={'black'} />
+          </Box>
+        </Box>
+      </Box>
 
       {/* List */}
 
